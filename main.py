f4bc204d01e0df8dd257bd50d126b725328b2b8c
RohaanJohn <rohaanrenujohn@gmail.com>
2023-04-15 09:57:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# speech to text
+
+import speech_recognition as sr
+ 
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    # audio = r.listen(source)
+    # make time limit 5 seconds
+    audio = r.listen(source, timeout=11)
+    print("Time over, thanks")
+    print ("Text: " + r.recognize_google(audio))
